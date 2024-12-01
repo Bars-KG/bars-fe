fa3208dd520bef6c4f6a1ad374562d4883ed96cf
Aidah Novallia Putri <aidahptr15@gmail.com>
2024-12-01 19:13:09 +0700

    feat: init search page

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,6 +1,9 @@
 export default function SearchResult() {
   return (
-    // <div className="flex flex-col w-full bg-gradient-to-t from-[#acdaff] to-[#D3EBFE]">tes</div>
-    <div className="flex flex-col w-full bg-gradient-to-t from-[#D3EBFE] to-white">tes</div>
+    <div className="flex w-full flex-col gap-10 bg-gradient-to-t from-[#D3EBFE] to-white lg:px-20 lg:py-32">
+      <span className="font-medium text-black">
+        Search result for <b className="text-[#37AAE8]">"Airport"</b>
+      </span>
+    </div>
   );
 }
