e74c9af0f67f0de0a60d51b49a1a9b0f7adf0257
Aidah Novallia Putri <aidahptr15@gmail.com>
2024-12-04 22:10:19 +0700

    feat: modify homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,12 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div>
-      <div className="relative w-full justify-items-center gap-8 bg-gradient-to-b from-[#7EB2F1] via-[#C9DFF9] to-[#F4F9FE] p-4 pb-20 sm:p-8">
-        <div className="relative z-40 mt-48 w-full items-center justify-center text-center sm:mt-24 sm:mt-48">
+    <div
+      style={{ backgroundImage: 'linear-gradient(to bottom, #7EB2F1, #C9DFF9, #F4F9FE, #F0F6FD, #7EB2F1)' }}
+      className="w-full"
+    >
+      <div className="relative w-full justify-items-center gap-8 p-4 pb-20 sm:p-8">
+        <div className="relative z-40 mt-48 w-full items-center justify-center text-center sm:mt-48">
           <h1 className="text-center font-grandstander text-5xl font-bold text-[#002A48] drop-shadow-xl sm:text-8xl">
             SkyWiki
           </h1>
@@ -15,17 +18,23 @@ export default function Home() {
             Discover insider secrets, real-time updates, and everything you need to know about airports worldwide—all in
             one place!
           </p>
-          <div className="mt-6 w-full items-center justify-items-center px-4 sm:mt-8 sm:px-48 md:px-96">
+          <div className="mt-6 w-full items-center justify-items-center px-4 sm:mt-8 sm:max-w-screen-md place-self-center">
             <SearchInput />
           </div>
         </div>
         <div className="absolute inset-0 right-0 z-30 mt-48 sm:mt-32">
-          <Image src="/images/cloud-bg.svg" alt="cloud elements" width={1440} height={773} className="object-cover" />
+          <Image
+            src="/images/cloud-bg.svg"
+            alt="cloud elements"
+            width={1440}
+            height={773}
+            className="object-cover 2xl:w-full"
+          />
         </div>
       </div>
 
-      <div className="relative min-h-screen w-full justify-items-center gap-16 bg-gradient-to-b from-[#F0F6FD] to-[#7EB2F1] p-4 pb-20 sm:p-8">
-        <div>
+      <div className="relative z-40 min-h-screen w-full justify-items-center gap-16 p-4 pb-20 sm:p-8">
+        <div className="flex w-full flex-col items-center justify-center overflow-hidden">
           <h2 className="pt-24 text-center font-grandstander text-2xl font-semibold text-[#002A48] drop-shadow-xl sm:pt-48 sm:text-3xl">
             What Can You Find Here?
           </h2>
