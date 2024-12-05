2ea35be31d3e78197788879cc99b349ec78a9edb
aidahputri <aidahptr15@gmail.com>
2024-12-05 14:09:43 +0700

    feat: mapping popular airports to homepage

--- src/app/airport/[id]/page.tsx
+++ src/app/airport/[id]/page.tsx
@@ -3,7 +3,7 @@ import api from '@/libs/axios/api';
 
 export default async function Airport({ params }: { params: Promise<{ id: string }> }) {
   const { id } = await params;
-  const res = await api.get(`airports/${id}`);
+  const res = await api.get(`airports/${id}/`);
   const data = res.data;
 
   return <Detail title={data.title} detail_fields={data.detail_fields} />;
